228b3f29938b4c02231127156541051737927f49
heechul <heechul4296@gmail.com>
2024-09-07 23:33:12 +0900

    setting project

---
+++ src/main/java/study/database/studydatabasejdbc/StudyDatabaseJdbcApplication.java
@@ -0,0 +1,13 @@
+package study.database.studydatabasejdbc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudyDatabaseJdbcApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StudyDatabaseJdbcApplication.class, args);
+    }
+
+}
---
+++ src/test/java/study/database/studydatabasejdbc/StudyDatabaseJdbcApplicationTests.java
@@ -0,0 +1,13 @@
+package study.database.studydatabasejdbc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudyDatabaseJdbcApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
